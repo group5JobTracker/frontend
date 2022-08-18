7e1c06d415231e8ae2d65f8d43181f982cc0eb7e
themarkr <markrodriguezaa@gmail.com>
2022-08-17 21:54:51 -0400

    display error message when unsuccessful login attempt is made

--- src/components/logInPage/LoginPage.js
+++ src/components/logInPage/LoginPage.js
@@ -1,6 +1,58 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import Context from '../../context/context';
 import iconLogo from "../../imgFiles/Dragonfly-Logo-Icon-Small.svg"
+
 const LogInPage = () => {
+    const navigate = useNavigate();
+    const [credentials, setCredentials] = useState({});
+    const [message, setMessage] = useState('');
+    let [attempts, updateAttempts] = useState(0);
+    const context = React.useContext(Context);
+    console.log(context)
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const userEmail = event.target.email.value;
+        const userPassword = event.target.password.value;
+        console.log(userEmail, userPassword);
+        console.log(credentials);
+        if(userEmail) {
+            setCredentials({
+                email : userEmail,
+                password : userPassword
+            })
+        }
+        updateAttempts(attempts+=1);
+    }
+
+    const loginAttempt = async(credentials) => {
+        const response = await fetch("https://dragonfly.herokuapp.com/auth/login", {
+            method : "POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify(credentials)
+        })
+        const data = await response.json()
+        console.log(data);
+        return data;
+    }
+
+    React.useEffect(() => {
+        if(attempts > 0) {
+            loginAttempt(credentials).then(data => {
+                if(data.message) {
+                    setMessage(data.message)
+                } else {
+                    setMessage('');
+                    context.updateUserInfo(data);
+                    // window.localStorage.setItem('user', data)
+                    navigate('/dashboard');
+                }
+            })
+        }
+    },[credentials])
 
     return (
         <div className="pageWrapper">
@@ -15,16 +67,17 @@ const LogInPage = () => {
             </div>
             <div className="center">
                 <div className="formContainer center">
-                    <h4>Sign up for free</h4>
-                    <form>
+                    <h4>Log in</h4>
+                    <form onSubmit={handleSubmit}>
                         <div className="center inputFields">
-                            <input type="text" id="email" name="Email" placeholder="   Email"/>
-                            <input type="text" id="password" name="Password" placeholder="   Password"/>
+                            <input type="text" id="email" name="email" placeholder="   Email"/>
+                            <input type="text" id="password" name="password" placeholder="   Password"/>
+                        </div>
+                        {message && <p>{message}</p>}
+                        <div className="center">
+                            <button className="signUpBtn" type="submit">Log in</button>
                         </div>
                     </form>
-                    <div className="center">
-                        <button className="signUpBtn">Log in</button>
-                    </div>
                 </div>
             </div>
             <div className="center">
