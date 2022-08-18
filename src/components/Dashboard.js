43ac1074ae308be041252375c9cc0f7dfc8310db
themarkr <markrodriguezaa@gmail.com>
2022-08-17 21:38:52 -0400

    add context to dashboard page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,15 @@
 //Libraries
-import { useState } from "react";
+import React, { useState } from "react";
 //Components
 import NewJobEntry from "./NewJobEntry"
 import JobList from "./JobList"
 import Nav from "./Nav"
-
+import Context from "../context/context";
 function Dashboard() {
-
+    const context = React.useContext(Context)
     const [newApplic, setNewApplic] = useState(false)
     const [cardViewToggle, setCardViewToggle] = useState(true)
-
+    console.log(context)
     return (
         <div className="dashboardStyle">
             <div className="wrapper">
