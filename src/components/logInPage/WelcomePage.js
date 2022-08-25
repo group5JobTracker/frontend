dad0ff462d8d74c9e759352a0960cab30039c00c
themarkr <markrodriguezaa@gmail.com>
2022-08-25 02:27:57 -0400

    add welcome page content

--- src/components/logInPage/WelcomePage.js
+++ src/components/logInPage/WelcomePage.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 import logoIconLarge from '../../imgFiles/Dragonfly-Logo-Icon-Large.svg';
 import './welcomePage.css';
 export default function WelcomePage() {
+    const navigate = useNavigate()
+    const currUser = window.localStorage.getItem("user")
+    const parsedUser = JSON.parse(currUser)
+    useEffect(() => {
+        setTimeout(() => {
+            navigate('/dashboard')
+        },3000)
+    })
   return (
     <div className='wrapPage'>
         <div className='welcomeContent'>
-            <img src={logoIconLarge} />
-            <p>Welcome, John Doe</p>
+            <img className = 'fadeInText'src={logoIconLarge} />
+            <p className='fadeInText'>Welcome, {parsedUser.first_name} {parsedUser.last_name}</p>
         </div>
     </div>
   )
