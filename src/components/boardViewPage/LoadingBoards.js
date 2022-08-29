e3fd5bf8aee78d99bb3c7f05ceaa9cdff6fea131
themarkr <markrodriguezaa@gmail.com>
2022-08-28 21:03:02 -0400

    styling added for loading boards state

---
+++ src/components/boardViewPage/LoadingBoards.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './loadingBoards.css'
+export default function LoadingBoards() {
+  return (
+    <div className='loadingSection'>
+        <div className='silhouette'>
+        </div>
+        <div className='silhouette'>
+        </div>
+        <div className='silhouette'>
+        </div>
+        <div className='silhouette'>
+        </div>
+    </div>
+  )
+}
