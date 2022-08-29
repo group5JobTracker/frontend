ec0a3fa1ccb668113224a26aaa1cf8f6b4ce4ba5
themarkr <markrodriguezaa@gmail.com>
2022-08-28 21:15:05 -0400

    add conditional styling for on hover and selected

--- src/components/boardViewPage/boardCard.js
+++ src/components/boardViewPage/boardCard.js
@@ -1,16 +1,35 @@
 import './boardCard.css';
+import React, { useState } from 'react'
+const BoardCard = ({ cardInfo, toBeRemovedCards, setToBeRemovedCards, cardSelectedStates, setCardSelectedStates }) => {
+    const [hoveringState, setHoveringState] = useState(false)
+    const handleDotClick = (e) => {
+        if(!(cardSelectedStates[e.target.id])) {
+            setToBeRemovedCards([...toBeRemovedCards,e.target.id])
+            setCardSelectedStates(prevState => ({...prevState, [e.target.id] : true}))
+        } else {
+            const removedArr = toBeRemovedCards.filter(card => card != e.target.id)
+            setToBeRemovedCards(removedArr);
+            setCardSelectedStates(prevState => ({...prevState, [e.target.id] : false}))
+        }
+    }
+    const handleHoverEnter = () => {
+        setHoveringState(true);
+    }
 
-const BoardCard = ({ cardInfo, showCol1, showCol2, showCol3, setCardEditModal }) => {
-    const formattedDate = cardInfo.created_at.split("T")[0]
-
+    const handleHoverExit = () => {
+        setHoveringState(false)
+    }
 
     return (
-        <div className="card" key={cardInfo.app_id}>
-            <div className="cardBody">
+        <div className="card" key={cardInfo.app_id} onMouseEnter={handleHoverEnter} onMouseLeave = {handleHoverExit} style={hoveringState ? { borderRadius: '10px', border: `2px solid ${cardInfo.card_color_hex}`}: {}}>
+            
+                <div className='cardHeader' id = {cardInfo.app_id}>
+                {(hoveringState || cardSelectedStates[cardInfo.app_id]) || toBeRemovedCards.length > 0? <span className='selectDot' id = {cardInfo.app_id} style={cardSelectedStates[cardInfo.app_id] ? {background : "#84FF4A"} : {background : "transparent"} } onClick= {handleDotClick}></span> : <span></span>}
                 <div className="cardTags">
-                    <p className="tagPill">Remote</p>
+                    <p className="tagPill" style ={cardInfo.tagName === "Remote" ? {background : "#4AC9FF"} : cardInfo.tagName === "Office" ? {background : "#FFE24A"} : {background : "#63BF37"}}id = {cardInfo.app_id}>{cardInfo.tagName}</p>
                 </div>
-
+              </div>
+              <div className="cardBody">
                 <div className="jobInfo">
                     <p className="title">{cardInfo.position}</p>
                     <p className="company">{cardInfo.company}</p>
@@ -18,13 +37,14 @@ const BoardCard = ({ cardInfo, showCol1, showCol2, showCol3, setCardEditModal })
                 </div>
 
                 <div className="jobStatus">
-                    <p>Date Applied: {formattedDate}</p>
+                    <p>Date Applied: {cardInfo.created_at}</p>
                     <p>Status: {cardInfo.status}</p>
                 </div>
             </div>
-            <div className="accentColor" style={{ background: cardInfo.card_color_hex }}></div>
+            <div className="accentColor" style={hoveringState ? { backgroundColor: "#23295e" } :{ background: cardInfo.card_color_hex }}></div>
         </div>
     )
 }
 
 export default BoardCard;
+
