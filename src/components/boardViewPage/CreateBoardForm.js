0dccc7d44ee5c8d31cc06ddf3a7a4f5388372676
themarkr <markrodriguezaa@gmail.com>
2022-08-28 21:13:51 -0400

    remove console.log statements

--- src/components/boardViewPage/CreateBoardForm.js
+++ src/components/boardViewPage/CreateBoardForm.js
@@ -3,17 +3,13 @@ import React, { useState } from 'react';
 import Context from '../../context/context';
 const CreateBoardForm = ({setSectionClicked, setNewBoard, newBoard}) => {
     //selected color for the board
-    const [selectedColor, setSelectedColor] = useState(null);
     const [newBoardInfo, setNewBoardInfo] = useState({});
     // context where user id is stored
-    const context = React.useContext(Context);
     const currUser = window.localStorage.getItem("user")
     const parsedUser = JSON.parse(currUser)
     const userToken = window.localStorage.getItem('token');
     // when a dot is clicked, update the selected color
-    const handleDotClick = (e) => {
-        setSelectedColor(e.target.id)
-    }
+
 
     const handleFormSubmit = (e) => {
         e.preventDefault()
@@ -23,7 +19,6 @@ const CreateBoardForm = ({setSectionClicked, setNewBoard, newBoard}) => {
         setNewBoardInfo({
             name : e.target.newBoardName.value,
             owner : parsedUser.user_id,
-            color : selectedColor
         })
     }
     // we will call this createBoard function inside of our useEffect hook
@@ -46,7 +41,6 @@ const CreateBoardForm = ({setSectionClicked, setNewBoard, newBoard}) => {
         // only make the request when a user selects a color
         if("name" in newBoardInfo) {
             createBoard(newBoardInfo).then(data => {
-                console.log(data)
                 setSectionClicked(false);
             })
         }
